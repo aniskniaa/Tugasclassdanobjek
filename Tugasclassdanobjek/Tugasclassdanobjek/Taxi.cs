468787ecebeb6d97a7c568e636c922764f16c3c1
aniskniaa <rnia578@gmail.com>
2020-03-21 14:43:27 +0700

    Praktikum3

---
+++ Tugasclassdanobjek/Tugasclassdanobjek/Taxi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugasclassdanobjek
+{
+    class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassanger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            Console.WriteLine("OnDuty : {0}", OnDuty);
+            Console.WriteLine("Num Passanger : {0}", NumPassanger);
+        }
+
+        public void PickUpPassanger()
+        {
+            Console.WriteLine("{0} Sedang Menjemput Penumpang", DriverName);
+        }
+
+        public void DropOffPassanger()
+        {
+            Console.WriteLine("{0} Selesai Mengantar Penumpang", DriverName);
+        }
+    }
+}
